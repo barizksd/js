279c9419d82f890ae82608ddc63d4852da229320
barizksd <barizksd@gmail.com>
2024-11-28 21:07:45 -0800

    update today class

--- first.js
+++ first.js
@@ -1,1 +1,4 @@
 console.log("Welcome to kerala to karntaka")
+console.log("kochi")
+console.log("mallus ")
+console.log("kanndaia")
---
+++ program1.js
@@ -0,0 +1,37 @@
+// x = "new program"
+// console.log(typeof(x));
+// y = 10;
+// console.log(y)
+
+// const x= "Bmw-class4"
+// console.log(x);
+
+// var y= "R15 v2"
+// console.log(y);
+
+
+
+// const x= "m2 4g"
+// console.log(x);
+
+// var y= "Mt 15"
+// console.log(y);
+
+// let z= 40
+// console.log(z);
+
+// z = "20"
+// console.log(z);
+
+// console.log(x);
+// const x="new"
+
+// console.log(y);
+// var y= 10
+
+// console.log(n);
+// let n ="newp"
+
+for(var i=1; i<=10; i++){
+    console.log(i);
+}
