5b8a05cd9ad7272ce5da31d4a5ec4e70505f6994
barizksd <barizksd@gmail.com>
2024-12-05 20:59:08 -0800

    today update

--- program1.js
+++ program1.js
@@ -139,14 +139,260 @@
 
 //     console.log(age>=18? "eligible for voting" : "Not eligible for voting")
 
-num1 =500;
-num2 =600;
+// num1 =500;
+// num2 =600;
 
-if(num1>=num2){
-    console.log(`Number one is largest number ${num1}`)
-}
-else{
-    console.log(`Number two is largest number ${num2}`)
-}
+// if(num1>=num2){
+//     console.log(`Number one is largest number ${num1}`)
+// }
+// else{
+//     console.log(`Number two is largest number ${num2}`)
+// }
 
-console.log(num1>=num2? `Number one is larges number ${num1}` : `Number two is largest number ${num2}`)
+// console.log(num1>=num2? `Number one is larges number ${num1}` : `Number two is largest number ${num2}`)
+
+// write a program to largest 3 given numbers 
+
+// num1=100;
+// num2=250;
+// num3=300;
+
+// if(num1 > num2 && num3 ){
+//     console.log(`Number 1 is largest ${num1}`)
+// }
+// else if(num1 && num2 > num3){
+//     console.log(` 2 cond number is  largest ${num2}`)
+// }
+// else if(num1 && num2 < num3){
+//     console.log(`3 number is larges `)
+// }
+
+// num1 =300;
+// num2= 250;
+// num3= 200;
+
+// if(num1 > num2 && num1 > num3){
+//     if(num2> num3){
+//         console.log(`seond largest number is ${num2}`);
+//     }
+//     else{ 
+//         console.log(`number one is second larges number ${num3}`);
+//     }
+// }
+
+// num1 =100;
+// num2= 250;
+// num3= 200;
+
+// if(num1> num2 && num1>num3){
+//     if(num2>num3){
+//         console.log(`second larges number id ${num2}`);
+//         console.log(`Descending order is ${num1} > ${num2} > ${num3}`)
+//     }
+//     else{
+//         console.log(`secondlarges number is ${num3}`);
+//         console.log(`Desecending order is ${num1}> ${num3} > ${num2}`)
+//     }
+// }
+// else if(num2> num1 && num2>num3){
+//     if(num1>num3){
+//         console.log(`second larges number id ${num1}`);
+//         console.log(`Descending order is ${num2} > ${num1} > ${num3}`)
+//     }
+//     else{
+//         console.log(`secondlarges number is ${num3}`);
+//         console.log(`Desecending order is ${num2}> ${num3} > ${num1}`)
+//     }
+// }
+// else if(num3> num1 && num3>num2){
+//     if(num1>num2){
+//         console.log(`second larges number id ${num1}`);
+//         console.log(`Descending order is ${num3} > ${num1} > ${num2}`)
+//     }
+//     else{
+//         console.log(`secondlarges number is ${num2}`);
+//         console.log(`Desecending order is ${num3}> ${num2} > ${num1}`)
+//     }
+// }
+// else(
+//     console.log(`the give 3 number are Equal`)
+// )
+
+// write a program javascript 3 times using while loop
+
+// i = 1;
+
+// while(i<=3){
+//     console.log("javascript");
+//     i++
+// }
+
+// write a program form 1 to 5 
+// write a program print number form 10 to 1 
+
+// i=1;
+
+// while (i<=5){
+//     console.log(i);
+//     i++
+// }
+// i=10;
+// while (i>=1){
+//     console.log(i);
+//     i--
+// }
+
+// write a program all even number between 1 to 50
+
+// i =0;
+
+// while(i<=50){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// write a program odd numbers 
+// i =0;
+
+// while(i<=50){
+//     if(i%2!=0){
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// write a program sum of 1 to 10
+
+// i = 0;
+// j =0;
+// while (i<=10){
+//     j+=i;
+//     i++;
+// }
+// console.log(j);
+
+// write a print the following out put for the given input 
+
+
+
+
+// input =3;
+// i=1;
+// str="";
+// pro=0;
+// while (i<=input){
+//     str = str +i;
+//     i++
+// }
+// pro = str * input;
+// console.log(pro);
+
+// input = 2;
+// i =1;
+// str ="";
+// sum = 0;
+
+// while(i<=input){
+//     str = str + input;
+//     sum = sum + Number(str);
+//     i++;
+// }
+// console.log(sum)
+
+// wtire a program the reverse of a given number 
+
+// num = 546;
+// ld = 0;
+// str ="";
+
+// while(num>0){
+//     ld = num%10;
+//     str = str + id;
+//     num = Math.floor(num/10);
+
+// }
+// console.log(str);
+
+
+// write a program check wherther a given numbe is pallindrom or not 
+
+// num = 121;
+// str = "";
+// ld = 0;
+// temp = num;
+
+// while(num>0){
+//     ld= num%10;
+//     str = str + ld;
+//     num = Math.floor(num/10);
+// }
+// console.log(temp==str? `the  given number is palidrom` : "the given number is  not palindrom");
+
+// num =4;
+// i = 1;
+// sum = 1;
+
+// while (num=>i){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+// write a program 3 digit number is amstrong or not
+    // sum of power of individual digiys
+    // number which is equal to the sum of its own digit each raised to the power of the number of digits
+
+    // num = 153;
+    // power = (num+"").length
+    // id = 0;
+    // arm =0;
+    // temp = num;
+
+    // while(num>0){
+    //     id = num%10;
+    //     arm = arm +id**power;
+    //     num = Math.floor(num/10);
+    // }
+    // console.log(temp == arm? `Number is Armstrong` : `The Number is not armstrong`);
+
+    // writ program display the numer whose exponential is in the range of 8 to 36. Where the power (exponential value) can be determined by the user.
+
+    // power =2;
+    // low = 8;
+    // up = 36;
+    // i =1;
+
+    // while(i<up){
+    //     if(low<=i**power && i**power<=up){
+    //         console.log(i);
+    //     }
+    //     i++
+    // }
+
+    // write a program to print number form 1 to 5
+
+    // for(i=1; i<=5; i++){
+    //     console.log(i)
+    // }
+
+    // write a program to print number form 10 to 11
+
+    // for(i=10; i>=1; i--){
+    //     console.log(i)
+    // }
+
+    // write a program create a for loop which have the capacity to iterate upto 10 shuld print till 5 only
+    // break used to break or exit fro the loop farcefully onluy ofr parent loop. can reduce no"of iteration
+
+
+    for(i=1; i<=10; i++){
+        console.log("valu")
+        if(i<=5){
+            console.log(i);
+        }
+    }
+
+
+    // write a program a gvien number is prime or not 
