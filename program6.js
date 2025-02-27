0af83d5b46e55d7cb93f89f85f2931260b58e5e9
barizksd <barizksd@gmail.com>
2025-02-27 01:44:22 -0800

    new update

--- program6.js
+++ program6.js
@@ -1,57 +1,57 @@
-accounts=[
-    {
-        acno:1000,ac_type:'savings',balance:45000,transaction:[
-            {
-                to:1001,amount:5000,msg:'ebill',mode:'gpay'
-            },
-            {
-                to:1002,amount:2000,msg:'emi',mode:'neft'
-            },
-            {
-                to:1003,amount:1000,msg:'recharge',mode:'phonePay'
-            },
-        ]
-    },
-    {
-        acno:1001,ac_type:'current',balance:30000,transaction:[
-            {
-                to:1000,amount:1000,msg:'grossary',mode:'gpay'
-            },
-            {
-                to:1002,amount:7000,msg:'gift',mode:'phonePay'
-            },
-            {
-                to:1003,amount:10000,msg:'emi',mode:'neft'
-            },
-        ]
-    },
-    {
-        acno:1002,ac_type:'fixed',balance:100000,transaction:[
-            {
-                to:1000,amount:5000,msg:'ebill',mode:'gpay'
-            },
-            {
-                to:1001,amount:2000,msg:'emi',mode:'neft'
-            },
-            {
-                to:1003,amount:1000,msg:'recharge',mode:'phonePay'
-            },
-        ]
-    },
-    {
-        acno:1003,ac_type:'savings',balance:30000,transaction:[
-            {
-                to:1001,amount:5000,msg:'ebill',mode:'gpay'
-            },
-            {
-                to:1002,amount:2000,msg:'emi',mode:'neft'
-            },
-            {
-                to:1000,amount:1000,msg:'recharge',mode:'phonePay'
-            },
-        ]
-    }
-]
+// accounts=[
+//     {
+//         acno:1000,ac_type:'savings',balance:45000,transaction:[
+//             {
+//                 to:1001,amount:5000,msg:'ebill',mode:'gpay'
+//             },
+//             {
+//                 to:1002,amount:2000,msg:'emi',mode:'neft'
+//             },
+//             {
+//                 to:1003,amount:1000,msg:'recharge',mode:'phonePay'
+//             },
+//         ]
+//     },
+//     {
+//         acno:1001,ac_type:'current',balance:30000,transaction:[
+//             {
+//                 to:1000,amount:1000,msg:'grossary',mode:'gpay'
+//             },
+//             {
+//                 to:1002,amount:7000,msg:'gift',mode:'phonePay'
+//             },
+//             {
+//                 to:1003,amount:10000,msg:'emi',mode:'neft'
+//             },
+//         ]
+//     },
+//     {
+//         acno:1002,ac_type:'fixed',balance:100000,transaction:[
+//             {
+//                 to:1000,amount:5000,msg:'ebill',mode:'gpay'
+//             },
+//             {
+//                 to:1001,amount:2000,msg:'emi',mode:'neft'
+//             },
+//             {
+//                 to:1003,amount:1000,msg:'recharge',mode:'phonePay'
+//             },
+//         ]
+//     },
+//     {
+//         acno:1003,ac_type:'savings',balance:30000,transaction:[
+//             {
+//                 to:1001,amount:5000,msg:'ebill',mode:'gpay'
+//             },
+//             {
+//                 to:1002,amount:2000,msg:'emi',mode:'neft'
+//             },
+//             {
+//                 to:1000,amount:1000,msg:'recharge',mode:'phonePay'
+//             },
+//         ]
+//     }
+// ]
 
 //1. total number of accounts
 
@@ -82,7 +82,7 @@ accounts=[
 
 //6. print credit transaction of account 1002
 
-  cread = accounts.map((data)=>data.transaction).flat().filter((item)=>item.to==1002) 
+//   cread = accounts.map((data)=>data.transaction).flat().filter((item)=>item.to==1002) 
 //   .forEach((item)=>console.log(item));
 //    console.log(cread);
    
@@ -90,23 +90,23 @@ accounts=[
 
 //7. print debit transaction of account 1002
 
-   ac = accounts.find((data)=>data.acno==1002).transaction
+//    ac = accounts.find((data)=>data.acno==1002).transaction
 //    console.log(ac);
    
 
 //8. print transaction history of 1002
     
-    his = {
-        creadit:cread,
-        debit:ac
-    }
+    // his = {
+    //     creadit:cread,
+    //     debit:ac
+    // }
    
-    console.log(his);
+    // console.log(his);
 
 // print history has an array
 
-    transhis = [...cread, ...ac]
-    console.log(transhis);
+    // transhis = [...cread, ...ac]
+    // console.log(transhis);
     
 
     
@@ -114,6 +114,7 @@ accounts=[
 
 //9. print highest balance account details
 
-   hy = accounts.reduce((ac1, ac2)=>ac1.balance>ac2.balance?ac1:ac2)
-   console.log(hy);
+//    hy = accounts.reduce((ac1, ac2)=>ac1.balance>ac2.balance?ac1:ac2)
+//    console.log(hy);
    
+
